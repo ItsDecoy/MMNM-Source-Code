163750c7458e5cd110301571f72992d23790e376
Ighby <igbhy05@gmail.com>
2022-08-08 11:07:58 -0300

    fixed Pissing
    Bottom Text

--- assets/preload/stages/The End.lua
+++ assets/preload/stages/The End.lua
@@ -4,17 +4,17 @@ function onCreate()
 	addLuaSprite('Sky', false);
 	scaleObject('Sky',1.8,1.8);
 
-	makeLuaSprite('Floor(to piss on)','piss/BG-Groud', -300, 550);
+	makeLuaSprite('Floor(to piss on)','piss/BG-groud', -300, 550);
 	addLuaSprite('Floor(to piss on)', false);
 	scaleObject('Floor(to piss on)', 1.2,1.2)
 
-	makeLuaSprite('Cloud','piss/Bg-Clouds', -300, -150);
+	makeLuaSprite('Cloud','piss/BG-Clouds', -300, -150);
 	addLuaSprite('Cloud', false);	
 	
-	makeLuaSprite('GoobShad','piss/Bg-Goombas-Shadow', 200, 700);
+	makeLuaSprite('GoobShad','piss/BG-Goombas-Shadow', 200, 700);
 	addLuaSprite('GoobShad', false);
 
-	makeLuaSprite('MarShad','piss/Bg-Mario-shadow', 720, 650);
+	makeLuaSprite('MarShad','piss/BG-Mario-shadow', 720, 610);
 	addLuaSprite('MarShad', false);
 
 	
