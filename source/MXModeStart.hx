9f8090d503adee3f17bde9bab8a9bbff0f4c1f4f
Ighby <igbhy05@gmail.com>
2022-08-28 02:40:47 -0300

    Mx Mode menu changes
    FT. THE MUSHROOM CURSOR YAY!!!!!!!!!!!11!!

--- source/MXModeStart.hx
+++ source/MXModeStart.hx
@@ -1,5 +1,6 @@
 package;
 
+import flixel.group.FlxGroup;
 import flixel.effects.FlxFlicker;
 import flixel.graphics.FlxGraphic;
 #if desktop
@@ -57,18 +58,23 @@ import flixel.util.FlxSave;
 import Achievements;
 import flixel.addons.display.FlxBackdrop;
 
+using StringTools;
+
 class MXModeStart extends MusicBeatState
 {
     static var mxModeSongList = ['Familiar', "Hero's Doom"];
 
     var path = 'mxstart/';
 
+    var options:Array<String> = ['Play Game', 'Exit'];
+    var optionSprGroup:FlxSpriteGroup;
+    var curSelect = 0;
     var title:FlxSprite;
+    var mushcursor:FlxSprite;
     var merio:FlxSprite;
-    var playGame:FlxSprite;
     var spriteScale = 3;
 
-    var tooLateForYou = false;
+    var acceptedYourFate = false;
 
     override function create()
     {
@@ -83,13 +89,30 @@ class MXModeStart extends MusicBeatState
         title.screenCenter();
         add(title);
 
-        playGame = new FlxSprite(title.x, title.y).loadGraphic(Paths.image(path + 'play_game', 'preload'));
-        playGame.setGraphicSize(Std.int(playGame.width * spriteScale));
-        playGame.updateHitbox();
-        playGame.scrollFactor.set();
-        playGame.x += 89 * spriteScale;
-        playGame.y += 144 * spriteScale;
-        add(playGame);
+        optionSprGroup = new FlxSpriteGroup();
+        add(optionSprGroup);
+
+        mushcursor = new FlxSprite(title.x, title.y).loadGraphic(Paths.image(path + 'cursor', 'preload'));
+        mushcursor.setGraphicSize(Std.int(mushcursor.width * spriteScale));
+        mushcursor.updateHitbox();
+        mushcursor.scrollFactor.set();
+        mushcursor.x += 72 * spriteScale;
+        add(mushcursor);
+
+        for (i in 0...options.length)
+        {
+            var name:String = options[i].toLowerCase().replace(' ', '_');
+            var spr = new FlxSprite(title.x, title.y).loadGraphic(Paths.image(path + name, 'preload'));
+            spr.setGraphicSize(Std.int(spr.width * spriteScale));
+            spr.updateHitbox();
+            spr.scrollFactor.set();
+            spr.x += 88 * spriteScale;
+            spr.y += (144 + (15 * i)) * spriteScale;
+            spr.ID = i;
+            optionSprGroup.add(spr);
+        }
+
+        mushcursor.y = optionSprGroup.members[0].y;
 
         merio = new FlxSprite(title.x, title.y).loadGraphic(Paths.image(path + 'falsehero_sprite', 'preload'));
         merio.setGraphicSize(Std.int(merio.width * spriteScale));
@@ -106,57 +129,94 @@ class MXModeStart extends MusicBeatState
 
     override function update(elapsed:Float)
     {
-        var back = controls.BACK && !tooLateForYou;
-        var accept = controls.ACCEPT && !tooLateForYou;
+        var up = controls.UI_UP_P && !acceptedYourFate;
+        var down = controls.UI_DOWN_P && !acceptedYourFate;
+        var back = controls.BACK && !acceptedYourFate;
+        var accept = controls.ACCEPT && !acceptedYourFate;
+
+        if (up)
+        {
+            curSelect -= 1;
+            if (curSelect < 0) curSelect = optionSprGroup.length - 1;
+            FlxG.sound.play(Paths.sound('scrollMenu_nes'));
+            mushcursor.y = optionSprGroup.members[curSelect].y;
+        }
+        if (down)
+        {
+            curSelect += 1;
+            if (curSelect >= optionSprGroup.length) curSelect = 0;
+            FlxG.sound.play(Paths.sound('scrollMenu_nes'));
+            mushcursor.y = optionSprGroup.members[curSelect].y;
+        }
 
 		if (back)
 		{
 			persistentUpdate = false;
-			FlxG.sound.play(Paths.sound('cancelMenu'));
+            FlxG.camera.visible = false;
+			FlxG.sound.play(Paths.sound('Bump'));
 			MusicBeatState.switchState(new MainMenuState());
 		}
 
         if (accept)
         {
-            PlayState.storyWeek = 0;
-			PlayState.storyPlaylist = mxModeSongList;
-			PlayState.isStoryMode = true;
-            PlayState.chartingMode = false;
-
-			var diffic = CoolUtil.getDifficultyFilePath(1);
-			if(diffic == null) diffic = '';
-
-			PlayState.storyDifficulty = 1;
-
-			PlayState.SONG = Song.loadFromJson(PlayState.storyPlaylist[0].toLowerCase() + diffic, PlayState.storyPlaylist[0].toLowerCase());
-			PlayState.campaignScore = 0;
-			PlayState.campaignMisses = 0;
-
-            playGame.color = FlxColor.YELLOW;
-
-            FlxG.sound.playMusic(Paths.sound('mxModeStart'), 1, false);
-            var dumbtime = FlxG.sound.music.length / 1000;
-
-            FlxTween.tween(title, {alpha: 0.1}, dumbtime);
-            FlxFlicker.flicker(playGame, dumbtime, 0.06, false, false, null);
-
-            FlxG.sound.music.onComplete = function()
+            switch (options[curSelect])
             {
-                title.visible = false;
-                merio.visible = false;
-                new FlxTimer().start(1.5, function(timer:FlxTimer)
-                {
-                    LoadingState.loadAndSwitchState(new PlayState());
-                });
-            };
+                case 'Play Game':
+                    PlayState.storyWeek = 0;
+                    PlayState.storyPlaylist = mxModeSongList;
+                    PlayState.isStoryMode = true;
+                    PlayState.chartingMode = false;
 
-            new FlxTimer().start(2, function(timer:FlxTimer)
-            {
-                merio.loadGraphic(Paths.image(path + 'falsehero_stare', 'preload'));
-            });
+                    var diffic = CoolUtil.getDifficultyFilePath(1);
+                    if(diffic == null) diffic = '';
 
-            tooLateForYou = true;
-            FlxTransitionableState.skipNextTransIn = true;
+                    PlayState.storyDifficulty = 1;
+
+                    PlayState.SONG = Song.loadFromJson(PlayState.storyPlaylist[0].toLowerCase() + diffic, PlayState.storyPlaylist[0].toLowerCase());
+                    PlayState.campaignScore = 0;
+                    PlayState.campaignMisses = 0;
+
+                    FlxG.sound.playMusic(Paths.sound('mxModeStart'), 1, false);
+                    var dumbtime = FlxG.sound.music.length / 1000;
+
+                    var dark = FlxColor.fromString('0x1A1A1A');
+                    FlxTween.color(title, dumbtime, FlxColor.WHITE, dark);
+
+                    for (spr in optionSprGroup)
+                    {
+                        if (spr.ID == curSelect)
+                        {
+                            spr.color = FlxColor.YELLOW;
+                            FlxFlicker.flicker(spr, dumbtime, 0.06, false, false, null);
+                        }
+                        else
+                        {
+                            FlxTween.color(spr, dumbtime, FlxColor.WHITE, dark);
+                        }
+                    }
+                    
+                    FlxG.sound.music.onComplete = function()
+                    {
+                        FlxG.camera.visible = false;
+                        new FlxTimer().start(1.5, function(timer:FlxTimer)
+                        {
+                            LoadingState.loadAndSwitchState(new PlayState());
+                        });
+                    };
+
+                    new FlxTimer().start(2, function(timer:FlxTimer)
+                    {
+                        merio.loadGraphic(Paths.image(path + 'falsehero_stare', 'preload'));
+                    });
+
+                    acceptedYourFate = true;
+                    FlxTransitionableState.skipNextTransIn = true;
+                case 'Exit':
+                    persistentUpdate = false;
+                    FlxG.camera.visible = false;
+                    FlxG.sound.play(Paths.sound('Bump'));
+                    MusicBeatState.switchState(new MainMenuState());
+            }
         }
 
         super.update(elapsed);
