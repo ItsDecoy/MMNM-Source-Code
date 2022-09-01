771894d6924f2cec4e7467155063ff587854cca6
Ighby <igbhy05@gmail.com>
2022-08-31 22:30:42 -0300

    FH stage optimized
    also made Lucas actually bop at the beat

--- assets/preload/stages/World 1....lua
+++ assets/preload/stages/World 1....lua
@@ -1,92 +1,74 @@
-local i = 25
-function onCreate()
+local i = 0
 
+function onCreate()
 	makeLuaSprite('Backdrop', 'falseHero/mxfalse_hero_bg_sky', -600, -300);	
-		addLuaSprite('Backdrop',false);
+	addLuaSprite('Backdrop',false);
 	
 	makeLuaSprite('Sir Strife','falseHero/Mr.Strife',350,300);
-		addLuaSprite('Sir Strife',false)
-		scaleObject('Sir Strife',0.8,0.8)
-		setScrollFactor('Sir Strife',0.8,0.8)
-
+	addLuaSprite('Sir Strife',false)
+	scaleObject('Sir Strife',0.8,0.8)
+	setScrollFactor('Sir Strife',0.8,0.8)
 	
 	makeLuaSprite('Sir Strife1','falseHero/Mr.Strife',1050,300);
-		addLuaSprite('Sir Strife1',false)
-		scaleObject('Sir Strife1',0.8,0.8)
-		setScrollFactor('Sir Strife1',0.8,0.8)
-
+	addLuaSprite('Sir Strife1',false)
+	scaleObject('Sir Strife1',0.8,0.8)
+	setScrollFactor('Sir Strife1',0.8,0.8)
 	
 	makeLuaSprite('Sir Strife2','falseHero/Mr.Strife',-300,250);
-		addLuaSprite('Sir Strife2',false)
-		scaleObject('Sir Strife2',0.8,0.8)
-		setScrollFactor('Sir Strife2',0.8,0.8)
+	addLuaSprite('Sir Strife2',false)
+	scaleObject('Sir Strife2',0.8,0.8)
+	setScrollFactor('Sir Strife2',0.8,0.8)
 	
-		makeLuaSprite('Hills','falseHero/mxfalse_hero_bg_hills',-600,150);
-		addLuaSprite('Hills',false);
-		scaleObject('Hills',0.55,0.55);
-		setScrollFactor('Hills',0.9,0.9)
+	makeLuaSprite('Hills','falseHero/mxfalse_hero_bg_hills',-600,150);
+	addLuaSprite('Hills',false);
+	scaleObject('Hills',0.55,0.55);
+	setScrollFactor('Hills',0.9,0.9)
 
-		makeLuaSprite('Bushes','falseHero/mxfalse_hero_bg_bushes',-500,58);
-		addLuaSprite('Bushes',false);	
-		scaleObject('Bushes',0.6,0.6);
-		setScrollFactor('Bushes',0.95,0.95);
+	makeLuaSprite('Bushes','falseHero/mxfalse_hero_bg_bushes',-500,58);
+	addLuaSprite('Bushes',false);	
+	scaleObject('Bushes',0.6,0.6);
+	setScrollFactor('Bushes',0.95,0.95);
 
 	makeLuaSprite('Floor','falseHero/mxfalse_hero_bg_floor',-500,65);
-		addLuaSprite('Floor',false);
-		scaleObject('Floor',0.6,0.6);
-		setScrollFactor('Floor',0.95,0.95)
+	addLuaSprite('Floor',false);
+	scaleObject('Floor',0.6,0.6);
+	setScrollFactor('Floor',0.95,0.95)
 
 	makeLuaSprite('Shadow1','falseHero/mxfalse_hero_bg_shador',-210,160);
-		addLuaSprite('Shadow1',false);
-		scaleObject('Shadow1',0.53,0.53);
-		
+	addLuaSprite('Shadow1',false);
+	scaleObject('Shadow1',0.53,0.53);
 
 	makeLuaSprite('Shadow2','falseHero/mxfalse_hero_bg_shador2',-730,-120);
-		addLuaSprite('Shadow2',false);
-		scaleObject('Shadow2',0.7,0.7);
-		
-
+	addLuaSprite('Shadow2',false);
+	scaleObject('Shadow2',0.7,0.7);
 
 	makeLuaSprite('DeadToadAlert','falseHero/mxfalse_hero_bg_pole',-1200,-350);
-		addLuaSprite('DeadToadAlert',false);
-		scaleObject('DeadToadAlert',0.9,0.9)
-		setScrollFactor('DeadToadAlert',0.9,0.9);
-				
+	addLuaSprite('DeadToadAlert',false);
+	scaleObject('DeadToadAlert',0.9,0.9)
+	setScrollFactor('DeadToadAlert',0.9,0.9);
+	
    	makeLuaSprite('DeadToadAlert2','falseHero/mxfalse_hero_bg_wall',-200,-170);
-		addLuaSprite('DeadToadAlert2',false);
-		scaleObject('DeadToadAlert2',0.8,0.8);
+	addLuaSprite('DeadToadAlert2',false);
+	scaleObject('DeadToadAlert2',0.8,0.8);
 
 	makeAnimatedLuaSprite('Lucas','falseHero/Lucas_FalseH', -100, 500);
-	addAnimationByPrefix('Lucas','idle','Lucas Dance',24,true);
-		addLuaSprite('Lucas', false);
-		scaleObject('Lucas', 0.8, 0.8);
-
-
-
-
+	addAnimationByPrefix('Lucas','idle','Lucas Dance',24,false);
+	addLuaSprite('Lucas', false);
+	scaleObject('Lucas', 0.8, 0.8);
 end
 
 function onUpdate(elapsed) 
-
-	daElapsed = elapsed * 30
-	i = i + daElapsed
+	i = i + elapsed
 	
-	setProperty('Sir Strife.y', (math.sin(i/300)*65) + 300);
-	yy = (math.sin(i/150)*15) - 300
+	setProperty('Sir Strife.y', (math.sin(i/2)*30) + 300)
+	setProperty('Sir Strife1.x', (math.sin(0.3 + i/2)*65) + 1400);
+	setProperty('Sir Strife2.y', (math.sin(0.5 + i/2)*30) + 500)
+end
 
-daElapsed = elapsed * 30
-	i = i + daElapsed
-	
-	setProperty('Sir Strife1.x', (math.sin(i/250)*65) + 1400);
-	yy = (math.sin(i/250)*45) - 1000
+function onCountdownTick(counter)
+	objectPlayAnimation('Lucas', 'idle', true)
+end
 
-daElapsed = elapsed * 30
-	i = i + daElapsed
-	
-	setProperty('Sir Strife2.y', (math.sin(i/350)*65) + 500)
-	yy = (math.sin(i/125)*35) - 500
-
-
-	
-          end
-
+function onBeatHit()
+	objectPlayAnimation('Lucas', 'idle', true)
+end
