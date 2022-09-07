91d5fef965d9e79a22936d43791fd68c840e4961
ComicTheHero <105177088+ComicTheHero@users.noreply.github.com>
2022-09-07 07:57:32 -0700

    hai (COMICS BACK BABY)
    - DaisEYE
    - Changed Discord presence

--- source/Discord.hx
+++ source/Discord.hx
@@ -17,7 +17,7 @@ class DiscordClient
 	{
 		trace("Discord Client starting...");
 		DiscordRpc.start({
-			clientID: "863222024192262205",
+			clientID: "1017065575140302959",
 			onReady: onReady,
 			onError: onError,
 			onDisconnected: onDisconnected
