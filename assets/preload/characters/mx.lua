03ddba42c2133401d4599342f0b5283571c7e0f6
ComicTheHero <105177088+ComicTheHero@users.noreply.github.com>
2022-09-09 16:10:15 -0700

    goodbye mx shake :(

--- assets/preload/characters/mx.lua
+++
@@ -1,7 +0,0 @@
-function opponentNoteHit(id,data,type,sus)
-triggerEvent('Screen Shake','0.1,0.07','0.1,0.007')
-
-triggerEvent('Add Camera Zoom','0.01','0.01')
-
-triggerEvent('Screen Shake','0.1,0.007','0.1,0.007')
-end
