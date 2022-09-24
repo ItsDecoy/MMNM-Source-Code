3fb031522466075cc3e25f98820c898c2a57cd53
Ighby <igbhy05@gmail.com>
2022-09-23 21:24:33 -0300

    New Custom Transition

--- source/CustomFadeTransition.hx
+++ source/CustomFadeTransition.hx
@@ -14,58 +14,86 @@ import flixel.FlxSubState;
 import flixel.FlxSprite;
 import flixel.FlxCamera;
 
-class CustomFadeTransition extends MusicBeatSubstate {
+class CustomFadeTransition extends MusicBeatSubstate
+{
 	public static var finishCallback:Void->Void;
+	
 	private var leTween:FlxTween = null;
 	public static var nextCamera:FlxCamera;
 	var isTransIn:Bool = false;
-	var transBlack:FlxSprite;
-	var transGradient:FlxSprite;
 
-	public function new(duration:Float, isTransIn:Bool) {
+	var rows:Int = 15;
+	var columns:Int = 27;
+
+	public function new(duration:Float, isTransIn:Bool)
+	{
 		super();
 
 		this.isTransIn = isTransIn;
 		var zoom:Float = CoolUtil.boundTo(FlxG.camera.zoom, 0.05, 1);
+		if (nextCamera != null) zoom = CoolUtil.boundTo(nextCamera.zoom, 0.05, 1);
 		var width:Int = Std.int(FlxG.width / zoom);
 		var height:Int = Std.int(FlxG.height / zoom);
-		transGradient = FlxGradient.createGradientFlxSprite(width, height, (isTransIn ? [0x0, FlxColor.BLACK] : [FlxColor.BLACK, 0x0]));
-		transGradient.scrollFactor.set();
-		add(transGradient);
 
-		transBlack = new FlxSprite().makeGraphic(width, height + 400, FlxColor.BLACK);
-		transBlack.scrollFactor.set();
-		add(transBlack);
+		for (i in 0...rows)
+		{
+			var yPos:Float = height * (i / rows);
+			for (j in 0...columns)
+			{
+				var xPos:Float = width * (j / columns);
+				var square = new FlxSprite(xPos-2, yPos-2).makeGraphic(Std.int(width/columns-1)+4, Std.int(height/rows-1)+4, FlxColor.BLACK);
+				square.scrollFactor.set();
 
-		transGradient.x -= (width - FlxG.width) / 2;
-		transBlack.x = transGradient.x;
+				var tweenDuration:Float = duration/(columns/2);
+				var startDelay:Float = duration-tweenDuration;
+				startDelay -= tweenDuration*j;
 
-		if(isTransIn) {
-			transGradient.y = transBlack.y - transBlack.height;
-			FlxTween.tween(transGradient, {y: transGradient.height + 50}, duration, {
-				onComplete: function(twn:FlxTween) {
-					close();
-				},
-			ease: FlxEase.linear});
-		} else {
-			transGradient.y = -transGradient.height;
-			transBlack.y = transGradient.y - transBlack.height + 50;
-			leTween = FlxTween.tween(transGradient, {y: transGradient.height + 50}, duration, {
-				onComplete: function(twn:FlxTween) {
-					if(finishCallback != null) {
-						finishCallback();
-					}
-				},
-			ease: FlxEase.linear});
+				var delA = duration/(rows/2);
+				var delB = duration-delA;
+				delB -= delA*i;
+				if (delB < 0) delB = -delB;
+				if (startDelay < 0) startDelay = -startDelay;
+				startDelay += delB;
+
+				startDelay /= 5;
+
+				if (isTransIn)
+				{
+					FlxTween.tween(square.scale, {x: 0, y: 0}, tweenDuration*8, {ease: FlxEase.linear, startDelay: startDelay,
+					onComplete: function(twn:FlxTween)
+					{
+						remove(square);
+						square.destroy();
+					}});
+				}
+				else
+				{
+					square.scale.x = 0;
+					square.scale.y = 0;
+					FlxTween.tween(square.scale, {x: 1, y: 1}, tweenDuration*8, {ease: FlxEase.linear, startDelay: startDelay});
+				}
+
+				if(nextCamera != null)
+				{
+					square.cameras = [nextCamera];
+				}
+				add(square);
+			}
 		}
 
-		if(nextCamera != null) {
-			transBlack.cameras = [nextCamera];
-			transGradient.cameras = [nextCamera];
-		}
+		new FlxTimer().start(duration, function(tm:FlxTimer)
+		{
+			if (isTransIn) close();
+			else 
+			{
+				if(finishCallback != null)
+					finishCallback();
+			}
+		}, 1);
 		nextCamera = null;
 	}
 
+	/*
 	override function update(elapsed:Float) {
 		if(isTransIn) {
 			transBlack.y = transGradient.y + transGradient.height;
@@ -78,7 +106,7 @@ class CustomFadeTransition extends MusicBeatSubstate {
 		} else {
 			transBlack.y = transGradient.y - transBlack.height;
 		}
-	}
+	}*/
 
 	override function destroy() {
 		if(leTween != null) {
