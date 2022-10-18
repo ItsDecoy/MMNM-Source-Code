f259299a1bef38d358c20a6c049a0eb6519486e5
ComicTheHero <105177088+ComicTheHero@users.noreply.github.com>
2022-10-18 13:32:23 -0700

    Stuff
    -Added Game Overs
    -Added MX Mode

---
+++ assets/preload/data/a-grand-ol'-time/game over.lua
@@ -0,0 +1,4 @@
+function onCreate()
+	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'Grand_dad_game_over');
+	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'grand_dad_game_over_end');
+end
